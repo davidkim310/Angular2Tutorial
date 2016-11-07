9f909638de37242333b40cbfc7907b281e46bf06
davidkim310 <davidkim310@gmail.com>
2016-11-07 11:06:29 -0800

    uses angular for dropdown bar

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,9 +7,12 @@ import { Recipe } from '../recipe';
   templateUrl: './recipe-list.component.html',
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+  new Recipe('Test', 'Description of test', 'http://vignette4.wikia.nocookie.net/mrmen/images/5/52/Small.gif/revision/latest?cb=20100731114437', []),
+  new Recipe('Dummy', 'Dummy', 'http://thumbs1.ebaystatic.com/d/l225/m/mfXELL6zPWJE4OC0agiXMZw.jpg', [])
+  ];
   @Output() recipeSelected = new EventEmitter<Recipe>();
-  recipe = new Recipe('Dummy', 'Description of Dummy', 'http://vignette4.wikia.nocookie.net/mrmen/images/5/52/Small.gif/revision/latest?cb=20100731114437')
+
   constructor() { }
   onSelected(recipe: Recipe) {
     this.recipeSelected.emit(recipe);
